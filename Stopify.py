78395f09d408f0be7c25738d3752026cb0b304b0
Michael Kipp <michaelkipp@outlook.com>
2017-07-20 10:18:07 -0500

    Allows users to create default stoplists

--- Stopify.py
+++ Stopify.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import argparse
+import heapq
+import random
+from collections import Counter
 from flask import Flask, render_template, jsonify, request
 from werkzeug.utils import secure_filename
 from TDMmaker import make_term_document_matrix
@@ -12,6 +15,7 @@ parser.add_argument("--corpus_path", help="path to corpus directory",
                     required=True)
 args = parser.parse_args()
 GLOBAL_TDM = None
+TERM_FREQ = Counter({})
 
 # TODO: Currently only handles .txt files
 def process_TDM():
@@ -24,12 +28,42 @@ def process_TDM():
     global GLOBAL_TDM
     GLOBAL_TDM = make_term_document_matrix(corpus_dir)
 
+def term_freq():
+    global TERM_FREQ
+    for item in GLOBAL_TDM:
+        TERM_FREQ[item] = 0
+    for item in GLOBAL_TDM:
+        for j in range(len(GLOBAL_TDM[item])):
+            TERM_FREQ[item] += int(GLOBAL_TDM[item][j])
+
+def create_stoplist(words, name):
+    try:
+        os.makedirs('stoplists/')
+    except OSError:
+        if not os.path.isdir('stoplists/'):
+            raise
+    with open("stoplists/" + name + ".txt", "w") as file:
+        for item in words:
+            print item
+            file.write(item + "\n")
+        file.close()
+
+@app.route('/default_stop/')
+def getStops():
+    global TERM_FREQ
+    frequentest = Counter({})
+    frequentest = TERM_FREQ.most_common(25)
+    default = []
+    for k, v in frequentest:
+        default.append(k)
+    create_stoplist(default, "default")
+    return jsonify({ 'stoplist':frequentest })
+
 # Render index.html
-
-
 @app.route('/')
 def home():
     process_TDM()
+    term_freq()
     return render_template('index.html',
                             num_docs=len(os.listdir(args.corpus_path)))
 
--- static/handler.js
+++ static/handler.js
@@ -1,3 +1,27 @@
+function generate_default_stoplist() {
+  var $stop = $("\
+      <div class='stop_holder'>    \
+        <div class='stop_box'>     \
+          <p>stop</p>              \
+        </div>                     \
+      </div>                       \
+  ");
+  return $stop;
+}
+
+d3.select('#def_stop_gen')
+  .on('click', function() {
+    console.log("Check");
+    var stopURL = '/default_stop/';
+    d3.json(stopURL, function(foo) {
+      console.log(foo);
+    });
+  });
+
+function add_default_stoplist() {
+  $(".stop_container").append(generate_default_stoplist());
+}
+
 function generateDoc(num) {
   var $doc = $("\
       <div class='doc_holder'>    \
