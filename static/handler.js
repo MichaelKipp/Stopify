d9ead00e20a1d94c67dd9a3fda67d1db42cf8d31
Michael Kipp <michaelkipp@outlook.com>
2017-07-20 16:31:12 -0500

    bleh

--- static/handler.js
+++ static/handler.js
@@ -1,7 +1,7 @@
 function generate_default_stoplist() {
   var $stop = $("\
-      <div class='stop_wrapper'>    \
-        <div class='stop_item'>     \
+      <div class='stop_wrapper'>   \
+        <div class='stop_item'>    \
           <p>stop</p>              \
         </div>                     \
       </div>                       \
@@ -18,15 +18,26 @@ d3.select('#def_stop_gen')
     });
   });
 
+// TODO: Stoplist listener
+// TODO: On hover reveal checkbox
+// TODO: Add icon to active stoplists
+// TODO: Add words to some data structure
+// TODO: Apply effects to corpus
+
+// TODO: Active stoplist listener
+// TODO: Updates corpus diagnostic visuals
+
+
+
 function add_default_stoplist() {
   $(".stop_container").append(generate_default_stoplist());
 }
 
 function generateDoc(num) {
   var $doc = $("\
-      <div class='doc_wrapper'>    \
-        <div class='doc_item'>     \
-          <p></p>      \
+      <div class='doc_wrapper'>   \
+        <div class='doc_item'>    \
+          <p></p>                 \
         </div>                    \
       </div>                      \
   ");
