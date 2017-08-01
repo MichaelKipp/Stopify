5751e5cbcff3aac1749fcfeb0af50a6bf38ee641
Michael Kipp <michaelkipp@outlook.com>
2017-08-01 16:39:44 -0500

    Sliding div for recommendations

--- Stopify.py
+++ Stopify.py
@@ -17,6 +17,12 @@ args = parser.parse_args()
 GLOBAL_TDM = None
 TERM_FREQ = Counter({})
 
+def get_doc_names():
+    docs = []
+    for item in os.listdir(args.corpus_path):
+        docs.append(item.strip(".txt"))
+    return docs
+
 # TODO: Currently only handles .txt files
 def process_TDM():
     if os.path.isdir(args.corpus_path):
@@ -36,6 +42,19 @@ def term_freq():
     for item in GLOBAL_TDM:
         for j in range(len(GLOBAL_TDM[item])):
             TERM_FREQ[item] += int(GLOBAL_TDM[item][j])
+
+# Return the contents of a document
+@app.route('/get_document/<name>')
+def getDocument(name):
+    contents = []
+    if os.path.isfile(args.corpus_path + name + ".txt"):
+        with open(args.corpus_path + name + ".txt", "r+") as file:
+            for line in file:
+                contents.append(line.strip())
+        contents = ''.join(contents)
+        return jsonify({ 'contents':contents })
+    else:
+        return "No such file exists."
 
 # TODO: Figure out how to do data right
 @app.route('/get_stoplist/<name>')
@@ -61,7 +80,23 @@ def createStoplist(name):
         print "File already exists"
     else:
         with open("stoplists/" + name + ".txt", "w") as file:
+            if name == 'default':
+                global TERM_FREQ
+                frequentest = Counter({})
+                frequentest = TERM_FREQ.most_common(25)
+                default = []
+                for k, v in frequentest:
+                    default.append(k)
+                for item in default:
+                    file.write("%s\n" % item)
             file.close()
+    return jsonify({})
+
+@app.route('/add_to_stoplist/<name>/<word>')
+def addToStoplist(name, word):
+    with open("stoplists/" + name + ".txt", "a+") as file:
+        file.write("%s\n" % word)
+        file.close();
     return jsonify({})
 
 # Render index.html
@@ -70,7 +105,7 @@ def home():
     process_TDM()
     term_freq()
     return render_template('index.html',
-                            num_docs=len(os.listdir(args.corpus_path)))
+                            doc_names = get_doc_names())
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/handler.js
+++ static/handler.js
@@ -1,5 +1,7 @@
 var activeStoplists = {};
 var activeStopwords = new Set();
+var pieChartData = [];
+var suggestionActive = false;
 
 // Engages a stoplist and adds it to active stops
 function activateStoplist(name) {
@@ -25,7 +27,6 @@ function activateStoplist(name) {
     });
   } else {
     // Clear html
-    console.log("#activeList_" + name +  "_wrapper");
     $("#activeList_" + name +  "_wrapper").remove();
     // Remove no longer active words
     var stopURL = '/get_stoplist/' + name;
@@ -36,7 +37,6 @@ function activateStoplist(name) {
     });
   }
   activeStoplists[name] = 1 - activeStoplists[name];
-  updateDocuments();
 }
 
 function logActiveStopwords() {
@@ -49,12 +49,37 @@ function createStoplist(name) {
   d3.select(".stop_page")
     .append("div")
       .attr("class", "item_wrapper stoplistWrapper")
+      .attr("id", "listwrapper_" + name)
     .append("div")
       .attr("class", "stop_item")
       .attr("id", "stoplist_" + name)
+      .on('mouseover', function() {
+          $('#list_' + name + "_options").show();
+      })
+      .on('mouseout', function() {
+          $('#list_' + name + "_options").hide();
+      })
       .on('click', function() {
         activateStoplist(name);
       })
+    .append("div")
+      .attr("class", "hover_options")
+      .attr("id", "list_" + name + "_options")
+  var hover = d3.select("#list_" + name + "_options")
+    hover.append("img")
+      .attr("class", "hover_icon")
+      .attr("src", "static/image/icons/add_gray.svg")
+      .attr("display", "block")
+      .attr("position", "relative")
+      .attr("margin", "0 auto")
+      .attr("width", "24px")
+      .attr("height", "24px")
+      .attr("fill", "red")
+      .on("click", function() {
+        addNewWords(name);
+      })
+
+  $('#list_' + name + "_options").hide();
   // Add to list of lists
   activeStoplists[name] = 0;
 
@@ -65,28 +90,111 @@ function createStoplist(name) {
   });
 }
 
+// TODO: Can I not call d3.json?
+// TODO: Turns off stoplist if its active. Check for this and reactivate if necessary
+function addNewWords(name) {
+  var newWord = prompt("Please enter a new stopword:", "");
+  var addURL = '/add_to_stoplist/' + name + '/' + newWord;
+  d3.json(addURL, function(foo) {
+
+  });
+  console.log(newWord);
+}
+
+function generatePieChartData(name) {
+  var documentURL = '/get_document/' + name;
+  d3.json(documentURL, function(d) {
+    text = d['contents'].split(" ");
+    active = 0;
+    inactive = 0;
+    for (i = 0; i < text.length; i++) {
+      if (activeStopwords.has(text[i])) {
+        active++;
+      } else {
+        inactive++;
+      }
+    }
+    pieChartData.push([active, inactive]);
+    generatePieChart(name);
+    pieChartData.pop();
+  })
+}
+
+// TODO: Add transition
+// TODO: Add labels
+function generatePieChart(name) {
+  var margin = 0,
+      radius = 25,
+      z = d3.scale.category20c();
+
+  var svg = d3.selectAll("#document_" + name).selectAll("svg")
+      .data(pieChartData)
+      .enter()
+    .append("svg")
+      .attr('width', (radius + margin) * 2)
+      .attr('height', (radius + margin) * 2)
+    .append("g")
+      .attr("transform", "translate(" + (radius + margin) + ","
+                                        + (radius + margin) + ")");
+
+  svg.selectAll("path")
+    .data(d3.layout.pie())
+    .enter()
+  .append("path")
+    .attr("d", d3.svg.arc()
+        .innerRadius(radius / 2)
+        .outerRadius(radius))
+    .style("fill", function(d, i) { return z(i); });
+}
+
 // Efficiently update document visuals to any changes to active stoplists
-function updateDocuments() {
-  // Remove specific items that changed instead of reloading whole visualization
-  // generateDocumentVisualizations();
+function updateVisuals() {
+  // Reset chart data
+  pieChartData = [];
+  // Make sure there are active stop words
+  if (activeStopwords.size > 0) {
+    for (i = 0; i < doc_names.length; i++) {
+      // createStats(doc_names[i]);
+      generatePieChartData(doc_names[i]);
+    }
+  } else {
+    $("svg").remove();
+  }
 }
 
 // Creates HTML for each doc in corpus
-function generateDocs(num) {
+function generateDocs(name) {
   d3.select(".doc_container")
     .append("div")
       .attr("class", "item_wrapper document_wrapper")
     .append("div")
       .attr("class", "doc_item")
+      .attr("id", "document_" + name)
       .on('click', function() {
-        // TODO: Make something happen.
+        var documentURL = '/get_document/' + name;
+        d3.json(documentURL, function(foo) {
+            // console.log(foo['contents']);
+        });
       })
 }
 
 // Adds documents from corpus to viewer
 function addDocs() {
-  for (i = 0; i < 5; i++) {
-    generateDocs(i);
+  for (i = 0; i < doc_names.length; i++) {
+    generateDocs(doc_names[i]);
+  }
+}
+
+function provideSuggestions() {
+  page = document.getElementById("suggestion_page")
+  if (suggestionActive = !suggestionActive)
+  {
+    page.style.boxShadow = "0px 0px 50px 0px rgba(2, 2, 2, .15)";
+    page.style.width =
+              (document.getElementById("doc_side").clientWidth - 20) + "px";
+  } else {
+    page.style.width = "0px";
+    page.style.boxShadow = "none";
   }
 }
 
